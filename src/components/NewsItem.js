8afb0750ddf769672c491db2866036eeea93bc0e
pranjalmittal01 <109210506+pranjalmittal01@users.noreply.github.com>
2023-09-26 19:29:49 +0530

    commited

---
+++ src/components/NewsItem.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
+    return (
+      <div className='my-3'>
+        <div className="card">
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
+            <img src={!imageUrl ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png" : imageUrl} className="card-img-top" alt="..." style={{height: '12rem'}}/>
+            <div className="card-body">
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
+                <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+            </div>
+            </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
+
+
+// import React from 'react'
+
+// const NewsItem = (props)=> {
+//         let { title, description, imageUrl, newsUrl, author, date, source } = props;
+//         return (
+//             <div className="my-3">
+//                 <div className="card">
+//                     {/* <div style={{
+//                         display: 'flex',
+//                         justifyContent: 'flex-end',
+//                         position: 'absolute',
+//                         right: '0'
+//                     }
+//                     }>  */}
+//                         <span className="badge rounded-pill bg-danger"> {source} </span>
+//                     {/* </div> */}
+
+//                     <img src={!imageUrl ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png" : imageUrl} className="card-img-top" alt="..." />
+//                     <div className="card-body">
+//                         <h5 className="card-title">{title}...</h5>
+//                         <p className="card-text">{description}...</p>
+//                         <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+//                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+     
+// }
+
+// export default NewsItem
